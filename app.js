df8083f21757c34e372bc16ef36362890eb21f34
T T <tomergk10@gmail.com>
2023-07-16 15:18:57 +0300

    Initital commit

---
+++ app.js
@@ -0,0 +1,155 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const port = 3000;
+
+const app = express();
+app.use(express.static('public'));
+app.use(bodyParser.json());
+
+let pass = process.env.PASSWORD;
+
+// ============= Connect =============
+async function connect() {
+  try {
+    await mongoose.connect(`mongodb+srv://admin-tomer:${pass}@cluster0.wg0hdfy.mongodb.net/placesDB`);
+  } catch (error) {
+    throw new Error(error);
+  }
+}
+connect();
+
+// ============= Schema =============
+const placeSchema = new mongoose.Schema({
+  name: String,
+  score: Number,
+  place: Number
+});
+
+// ============= Model =============
+const Place = mongoose.model('Place', placeSchema);
+
+// ============= Default Documents =============
+const contender1 = new Place({
+  name: "Ravit",
+  score: 3,
+  place: 1
+});
+
+const contender2 = new Place({
+  name: "PitzPitz",
+  score: 2,
+  place: 2
+});
+
+const contender3 = new Place({
+  name: "Mini",
+  score: 1,
+  place: 3
+});
+
+// ============= Insert default documents To default Array =============
+const defaultItems = [contender1, contender2, contender3];
+
+// ============= If current collection is empty => insert default array (should be inserted only once) =============
+Place.countDocuments({}, function (err, count) {
+  console.log('============================================================');
+  console.log(`Number of documents inside collection is: ${count}`);
+  if (count < 3) {
+    Place.insertMany(defaultItems, function (error, docs) {
+      if (!error) {
+        console.log('inserted default items to collection');
+      }
+      else {
+        console.error(`the error occurs when trying to insert docs when collection is empty, error is: ${error}`);
+      }
+    });
+  }
+  startServer();
+});
+
+// ============= GET REQUESTS to use in game_logic.js file, in order to fetch data =============
+app.get("/api/place/1", (req, res) => {
+  Place.findOne({ place: 1 }).exec()
+    .then((firstPlace) => {
+      res.json(firstPlace); // Send the retrieved data as a response
+    })
+    .catch((error) => {
+      res.status(500).json({ error: "Internal server error" });
+    });
+});
+
+app.get("/api/place/2", (req, res) => {
+  Place.findOne({ place: 2 }).exec()
+    .then((firstPlace) => {
+      res.json(firstPlace); // Send the retrieved data as a response
+    })
+    .catch((error) => {
+      res.status(500).json({ error: "Internal server error" });
+    });
+});
+
+app.get("/api/place/3", (req, res) => {
+  Place.findOne({ place: 3 }).exec()
+    .then((firstPlace) => {
+      res.json(firstPlace); // Send the retrieved data as a response
+    })
+    .catch((error) => {
+      res.status(500).json({ error: "Internal server error" });
+    });
+});
+
+// ============= POST REQUEST =============
+
+/* This post request gets two parameters from client side: name & score,
+ It finds all documents from DB and update database with the new contender. */
+app.post("/", async (req, res) => {
+  try {
+    // Getting user name & score
+    const { newName, newScore } = req.body;
+
+    // Retrieving DB documents
+    let firstDoc = await Place.findOne({ place: 1 }).exec();
+    let secondDoc = await Place.findOne({ place: 2 }).exec();
+    let thirdDoc = await Place.findOne({ place: 3 }).exec();
+
+    // Performing a comparison logic + update the right docs
+
+    if (newScore > secondDoc.score) {
+
+      if (newScore > firstDoc.score) { // updating first place, second place, third place
+        await Place.findOneAndUpdate({ place: 1 }, { $set: { name: newName, score: newScore } });
+        await Place.findOneAndUpdate({ place: 2 }, { $set: { name: firstDoc.name, score: firstDoc.score } });
+        await Place.findOneAndUpdate({ place: 3 }, { $set: { name: secondDoc.name, score: secondDoc.score } });
+      }
+
+      else { // updating second place, third place 
+        await Place.findOneAndUpdate({ place: 2 }, { $set: { name: newName, score: newScore } });
+        await Place.findOneAndUpdate({ place: 3 }, { $set: { name: secondDoc.name, score: secondDoc.score } });
+      }
+    }
+
+    else { // updating third place
+      await Place.findOneAndUpdate({ place: 3 }, { $set: { name: newName, score: newScore } });
+    }
+
+    // Send a success response
+    res.status(200).json({ success: true });
+  }
+
+  catch (error) {
+    // Handle any errors and send an error response
+    console.error(error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
+
+
+// ============= Listen =============
+function startServer() {
+  app.listen(port, () => {
+    console.log('Server started on port ' + port);
+  });
+}
---
+++ public/js/game_logic.js
@@ -0,0 +1,200 @@
+const buttonColors = ["red", "blue", "purple", "orange"]; // Options to pick from.
+let gamePattern = []; // Array which saves the colors that were picked randomly, in the right order.
+let userClickedPattern = []; // Array which saves the colors that the user picked.
+let level = 0; // The game starts at level 1 but for the easy logic it starts at 0.
+let started = false; // Flag which turn true after first key clicked in order to start the game.
+let currentUserName = "";
+let formSubmitted = true;
+let lowestScoreOnTable; // We always saves the lowest score on scores table
+
+// ============= Retrieving data from DB & display it on user's screen's table =============
+function retrieve() {
+  fetch("/api/place/1")
+    .then((response) => response.json())
+    .then((data) => {
+      // Updating HTML using retrieved data
+      document.getElementById("place1").innerHTML = data.place;
+      document.getElementById("name1").innerHTML = data.name;
+      document.getElementById("score1").innerHTML = data.score;
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+
+  fetch("/api/place/2")
+    .then((response) => response.json())
+    .then((data) => {
+      // Updating HTML using the retrieved data
+      document.getElementById("place2").innerHTML = data.place;
+      document.getElementById("name2").innerHTML = data.name;
+      document.getElementById("score2").innerHTML = data.score;
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+
+  fetch("/api/place/3")
+    .then((response) => response.json())
+    .then((data) => {
+      // Updating HTML using the retrieved data
+      document.getElementById("place3").innerHTML = data.place;
+      document.getElementById("name3").innerHTML = data.name;
+      document.getElementById("score3").innerHTML = data.score;
+      lowestScoreOnTable = data.score;
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+}
+
+function playSound(name) {
+  var audio = new Audio("/sounds/" + name + ".mp3");
+  audio.play();
+}
+
+function animatePress(currentColor) {
+  $("#" + currentColor).addClass("pressed");
+  setTimeout(function () {
+    $("#" + currentColor).removeClass("pressed");
+  }, 100);
+}
+
+// Picks next color + change title text level
+function nextSequence() {
+  userClickedPattern = []; // new every time  
+  level++;
+  $("#level-title").text("Level " + level);
+  var randomNumber = Math.floor(Math.random() * 4);
+  var randomChosenColor = buttonColors[randomNumber];
+  gamePattern.push(randomChosenColor);
+  $("#" + randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
+  playSound(randomChosenColor);
+}
+
+function prepareTostartOver() {
+  level = 0;
+  gamePattern = [];
+  started = false;
+}
+
+function showForm() {
+  document.getElementById('popup-form').classList.remove('form-off');
+  formSubmitted = false;
+  document.getElementById('level-title').innerHTML = '<h1 style="font-size: 70px; color: #F86F03">Good Game!</h1>'; // Change header
+  document.getElementsByClassName('footer')[0].classList.add('page-blur'); // blur the footer
+  document.getElementsByClassName('scores_board')[0].classList.add('page-blur'); // blur the scores board
+  document.getElementsByClassName('color_board')[0].classList.add('page-blur'); // blur the color board
+}
+function hideForm() {
+  document.getElementById('popup-form').classList.add('form-off');
+  formSubmitted = true;
+  document.getElementsByClassName('footer')[0].classList.remove('page-blur'); // blur the footer
+  document.getElementsByClassName('scores_board')[0].classList.remove('page-blur'); // blur the scores board
+  document.getElementsByClassName('color_board')[0].classList.remove('page-blur'); // blur the color board
+}
+
+async function getUserName() {
+  return new Promise((resolve) => {
+    document.getElementsByClassName("form_button")[0].addEventListener('click', function (event) {
+      event.preventDefault(); // Prevent the default form submission
+      resolve();
+    });
+  });
+}
+
+async function playSuccessSounds() {
+  // Functions that plays the success sounds
+  function playSounds(soundList) {
+    function playNextSound(index) {
+      if (index >= soundList.length) {
+        return;
+      }
+      const audio = new Audio("/sounds/" + soundList[index] + ".mp3");
+      audio.play();
+
+      audio.addEventListener("ended", function () {
+        playNextSound(index + 1);
+      });
+    }
+    playNextSound(0);
+  }
+  let list = ["success", "applause"];
+  playSounds(list);
+}
+
+// Function activates win effects + get user name + make a post request to server with user name & score.
+async function ifBetterScore() {
+  playSuccessSounds();
+  showForm();
+  await getUserName(); // Wait for the user to submit the form
+  currentUserName = document.getElementById("name-input").value;
+  document.getElementById("name-input").value = ""; // Clear the input value to prepare for the next click
+  
+  let contender = { newName: currentUserName, newScore: level - 1 };
+
+  fetch('/', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(contender)
+  })
+
+  setTimeout(() => {
+    retrieve();
+    $("#level-title").text("Please Press Any Key To Start The Game");
+    prepareTostartOver();
+    hideForm();
+  }, 1000);
+};
+
+function checkAnswer(currentLevel) {
+  if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+    if (userClickedPattern.length === gamePattern.length) {
+      setTimeout(function () {
+        nextSequence();
+      }, 1000);
+    }
+  }
+  else {
+    // checking if current score > third place on table's scores
+    if (level - 1 > lowestScoreOnTable) {
+      ifBetterScore(); // User failed => checking if score is better than any table's scores
+    }
+    else {
+      playSound("wrong");
+      $("#level-title").text("Game over");
+      setTimeout(function () {
+        $("#level-title").text("Please Press Any Key To Start The Game");
+      }, 4000);
+
+      prepareTostartOver();
+    }
+  }
+}
+
+// ========== WORK STARTS HERE ==========
+hideForm();
+retrieve();
+
+// Keypress function to start the game
+$(document).keypress(function () {
+  if (!started && formSubmitted) {
+    $("#level-title").text("Level " + level);
+    nextSequence();
+    started = true;
+  }
+});
+
+// Activate effects when the user click on a color
+$(".btn").click(function () {
+  if (formSubmitted) {
+    var userChosenColor = $(this).attr("id");
+    userClickedPattern.push(userChosenColor);
+    playSound(userChosenColor);
+    animatePress(userChosenColor);
+    checkAnswer(userClickedPattern.length - 1);
+  }
+});
+
+
